6df3c43856260e5faaf9394be1586562ebf7afba
Niihara Megumu <nhrmgm1223@gmail.com>
2022-06-19 16:08:14 +0900

    add: post list

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -9,6 +9,8 @@
 
 import { Router, Route, Set } from '@redwoodjs/router'
 
+import PostsLayout from 'src/layouts/PostsLayout'
+
 import BlogLayout from './layouts/BlogLayout/BlogLayout'
 import AboutPage from './pages/AboutPage/AboutPage'
 import HomePage from './pages/HomePage/HomePage'
@@ -16,10 +18,20 @@ import HomePage from './pages/HomePage/HomePage'
 const Routes = () => {
   return (
     <Router>
+      <Route path="/article/{id:Int}" page={ArticlePage} name="article" />
+
+      <Set wrap={PostsLayout}>
+        <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
+        <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
+        <Route path="/posts/{id:Int}" page={PostPostPage} name="post" />
+        <Route path="/posts" page={PostPostsPage} name="posts" />
+      </Set>
+
       <Set wrap={BlogLayout}>
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
+
       <Route notfound page={NotFoundPage} />
     </Router>
   )
